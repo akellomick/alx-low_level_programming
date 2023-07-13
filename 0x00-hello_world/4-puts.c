f1501ce0337c85080e7313767facfbd65c660782
akellomick <mickakello@gmail.com>
2023-07-13 13:44:49 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * return:0 == no error
+ */
+int main(void)
+{
+	char msg[] = "Programming is like building a multilingual puzzle";
+
+	puts(msg);
+
+	return 0;
+}
+
