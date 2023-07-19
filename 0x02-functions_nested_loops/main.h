2d5e804e572323e9f8fe9f4a1576279c0ea174bb
akellomick <mickakello@gmail.com>
2023-07-19 11:45:21 -0700

    5. Sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,9 +4,11 @@
  * prototype for print_alphabet_x10()
  * prorotype for 3-islower.c
  * prototype for 4-isalpha
+ * prototype for 5-sign.c
  */
 int _putchar (char c);
 char print_alphabet();
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
