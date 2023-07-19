69b6ffe7331b87729f2fd881c8d7cf6d12b17ccf
akellomick <mickakello@gmail.com>
2023-07-19 11:12:37 -0700

    4. isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,10 @@
  * function prototype for 1-alphabet.c
  * prototype for print_alphabet_x10()
  * prorotype for 3-islower.c
+ * prototype for 4-isalpha
  */
 int _putchar (char c);
 char print_alphabet();
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
