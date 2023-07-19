27cedf3a3b094ca11f48ea00e4fd4d7080e703da
akellomick <mickakello@gmail.com>
2023-07-18 17:48:21 -0700

    2. 10 x alphabet_modified

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 	{
 		char alphabet;
 
-		for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			putchar(alphabet);
 		}
