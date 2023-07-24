a6b1cf42318916a7a83460a394cda9f096ba69cd
akellomick <mickakello@gmail.com>
2023-07-24 11:48:23 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates value of a pointer to an int.
+ * @n:Pointer to an int that is updated.
+ *
+ * Return: updated value to *n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+/**
+ * .h file: contains all 0x05 task prototypes.
+ */
+void reset_to_98(int *n);
