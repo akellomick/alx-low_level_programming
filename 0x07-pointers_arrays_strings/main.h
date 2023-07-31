87940e2029b24269881d318a9d3eba6068785da4
akellomick <mickakello@gmail.com>
2023-07-31 12:19:27 -0700

    Revert "1. memcpy"
    This reverts commit f3bbf23cdc2da90bdb83ff0a8faac77e1e233df1.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,4 +2,3 @@
  * prototypes for project - 0x07. C - Even more pointers, arrays and strings.
  */
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
