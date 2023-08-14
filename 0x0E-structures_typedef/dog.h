a4a4901bc82a7d54b1175145667ab6148e3e5d4c
akellomick <mickakello@gmail.com>
2023-08-14 14:36:48 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines elements of type dog.
+ * @name: first element.
+ * @age: second element.
+ * @owner:third element.
+ *
+ * Description: defines elements name, age and owner of type dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
