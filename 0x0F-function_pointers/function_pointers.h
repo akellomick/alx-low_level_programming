2e6c969ba9c32abf95d7eb4f586981ac45fd364f
akellomick <mickakello@gmail.com>
2023-08-16 23:23:42 -0700

    function_pointers_M

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#include <stdlib.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
