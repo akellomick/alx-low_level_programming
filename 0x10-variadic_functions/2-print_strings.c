8748addd3fde48a9d2988114b14ea9d83dc7802b
akellomick <mickakello@gmail.com>
2023-08-18 04:33:41 -0700

    2-print_strings.c_modofied

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (word != NULL)
 			printf("%s", word);
 		else
-			printf("nil");
+			printf("(nil)");
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
