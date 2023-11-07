72a1eaa1ee323c20b59a08765f5db0bd82656bd9
akellomick <mickakello@gmail.com>
2023-11-07 04:03:18 -0800

    Define a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog- type struct dog
+ * @name: firstMember
+ * @age: secondMember
+ * @owner: thirdMember
+ *
+ * Description: defines a tupe struct dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
